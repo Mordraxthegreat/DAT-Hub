5c59c51e44be7f1f0e5bc221aafbd17a5d5da37a
Mordrax <s1cvel.pro@gmail.com>
2025-08-27 19:27:22 +0200

    Update Mine.lua

--- Mine.lua
+++ Mine.lua
@@ -1,2 +1,14 @@
 print("get new one in the discord link copied")
-setclipboard("https://discord.gg/JjWbKxJ6ew")
+setclipboard('https://discord.gg/JjWbKxJ6ew')
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Vanguard Hub", 
+    Text = "this script is not work \nget new in discord server" ,
+    Icon = "rbxassetid://83737997498468",
+    Duration = 1
+})
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Vanguard Hub", 
+    Text = "Link Copied" ,
+    Icon = "rbxassetid://83737997498468",
+    Duration = 7
+})
